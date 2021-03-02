0669dffb7aeb54f7fdea2c26f2179637d74102d9
JoelDeoKL <joeldeocm88@gmail.com>
2021-03-02 22:34:05 +0100

    Mise en place de Model Projet

--- app/Projet.php
+++ app/Projet.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Projet extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = "projets";
+    public $timestamps = true;
+
+    protected $casts = [
+        'cost' => 'float'
+    ];
+
+    protected $guarded = [];
+
 }
